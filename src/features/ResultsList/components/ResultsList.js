d7d8c6ccdc1926fdd9e07a2a90c5998a7c4f509e
eyung <effiamyung@gmail.com>
2023-06-09 10:59:27 -0700

    lexname to display in results

--- src/features/ResultsList/components/ResultsList.js
+++ src/features/ResultsList/components/ResultsList.js
@@ -6,7 +6,7 @@ function ResultsList({ results }) {
       <h1>API Results</h1>
       <ul>
         {results.map((result, index) => (
-          <li key={index}>{result.word} - {result.lexFilenum}</li>
+          <li key={index}>{result.word} - {result.lexFilenum} - {results.lexname}</li>
         ))}
       </ul>
     </div>
