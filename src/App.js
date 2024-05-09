c4e179eb8c52c1093e7088d5b54d4fe565a25ecb
eyung <effiamyung@gmail.com>
2024-05-09 13:58:13 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const ScoreCard = ({ title, score, tooltiptext }) => (
   </div>
 );
 
+
 const ScoreCard2 = ({ title, scores, tooltiptext }) => (
   <div className="relative card p-2 bg-white shadow-sm rounded-lg">
     <div className="absolute top-0 right-0 p-1">
@@ -30,7 +31,7 @@ const ScoreCard2 = ({ title, scores, tooltiptext }) => (
     <div className="card-body p-6">
       <h2 className="text-l font-semibold text-gray-800">{title}</h2>
       {scores && Object.entries(scores).map(([word, score]) => (
-        <p key={word} className="mt-2 text-sm text-gray-600">{word}: {score}</p>
+        <p key={word} className="mt-2 text-sm text-gray-600">{`${word}: ${score}`}</p>
       ))}
     </div>
   </div>
