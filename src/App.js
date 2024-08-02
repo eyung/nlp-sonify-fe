d61ad59b75ad0b570e16d1a8e82492b3d1a47c86
eyung <effiamyung@gmail.com>
2024-08-02 10:23:06 -0700

    typo

--- src/App.js
+++ src/App.js
@@ -7,24 +7,25 @@ import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
 import SoundPlayer from './SoundPlayer';
 import ScoreMapper from './ScoreMapper';
 
-const ScoreCard = ({ title, scores, tooltiptext }) => (
+const ScoreCard = ({ title, scores, tooltiptext }) => {
   console.log('Rendering ScoreCard with scores:', scores);
   return (
-  <div className="relative card p-2 bg-white shadow-sm rounded-lg">
-    <div className="absolute top-0 right-0 p-1">
-      <div className="tooltip">
-        <i className="fas fa-question-circle text-gray-400"></i>
-        <span className="tooltiptext bg-gray-100 text-gray-700 p-2 rounded-md shadow-lg">{tooltiptext}</span>
+    <div className="relative card p-2 bg-white shadow-sm rounded-lg">
+      <div className="absolute top-0 right-0 p-1">
+        <div className="tooltip">
+          <i className="fas fa-question-circle text-gray-400"></i>
+          <span className="tooltiptext bg-gray-100 text-gray-700 p-2 rounded-md shadow-lg">{tooltiptext}</span>
+        </div>
+      </div>
+      <div className="card-body p-6">
+        <h2 className="text-l font-semibold text-gray-800">{title}</h2>
+        {scores && Object.entries(scores).map(([word, score]) => (
+          <p key={word} className="mt-2 text-sm text-gray-600">{`${word}: ${score}`}</p>
+        ))}
       </div>
     </div>
-    <div className="card-body p-6">
-      <h2 className="text-l font-semibold text-gray-800">{title}</h2>
-      {scores && Object.entries(scores).map(([word, score]) => (
-        <p key={word} className="mt-2 text-sm text-gray-600">{`${word}: ${score}`}</p>
-      ))}
-    </div>
-  </div>
-);
+  );
+};
 
 const Draggable = ({ id, children }) => {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
