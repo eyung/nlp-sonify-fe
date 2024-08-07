fe1ac684585a8650b45a21df31c7d6144d60468f
eyung <effiamyung@gmail.com>
2024-08-06 17:07:34 -0700

    Instead of manually updating the position state, use the transform property provided by the hook

--- src/Draggable.js
+++ src/Draggable.js
@@ -1,28 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDraggable } from '@dnd-kit/core';
 
 const Draggable = ({ id, children }) => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
   const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
     id,
-    onDragMove: ({ delta }) => {
-      setPosition((prevPosition) => {
-        const newPosition = {
-          x: prevPosition.x + delta.x,
-          y: prevPosition.y + delta.y,
-        };
-        console.log('New Position:', newPosition);
-        return newPosition;
-      });
-    },
   });
 
   const style = {
-    transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
+    transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
     backgroundColor: isDragging ? 'lightgreen' : 'transparent', // Change background color when dragging
   };
 
-  console.log('Render Draggable:', { position, isDragging });
+  console.log('Render Draggable:', { transform, isDragging });
 
   return (
     <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
