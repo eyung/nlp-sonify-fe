7fa76d4893a1e20d302c976288532767b28d9864
eyung <effiamyung@gmail.com>
2024-08-26 12:24:28 -0700

    revert

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const Root = () => {
   }, [isLoading]);
 
   return (
-    <div className={`App ${isLoading ? 'gradient-animation' : ''}`} style={backgroundStyle}>
+    <div className={`App ${isLoading ? 'gradient-animation' : ''}`}>
       <App setIsLoading={setIsLoading} />
     </div>
   );
