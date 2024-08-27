19ba6a4ac2d392f6360ca33c4def456cb1068a45
eyung <effiamyung@gmail.com>
2024-08-27 09:53:19 -0700

    modify the ScoreMapper component to transform scoresData

--- src/ScoreMapper.js
+++ src/ScoreMapper.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import { useScores } from './ScoreContext';
 
 // ScoreMapper component
-const ScoreMapper = ({ scores, mappings, children }) => {
+const ScoreMapper = ({ mappings, children }) => {
+  const { scoresData } = useScores();
 
-    // Map scores to the desired parameters
-    const mappedScores = scores.map(score => {
-      const mappedScore = {};
+  // Map scores to the desired parameters
+  const mappedScores = scoresData.map(score => {
+    const mappedScore = {};
 
-      // Map each score 
-      for (const [key, value] of Object.entries(score)) {
-        if (mappings[key]) {
-            //console.log(`Mapping key: ${key}, value: ${value}`);
-            //console.log(`Mapping function: ${mappings[key].mapFunction}`);
-          mappedScore[mappings[key].parameter] = mappings[key].mapFunction(value);
-        } else {
-          mappedScore[key] = value;
-        }
+    // Map each score 
+    for (const [key, value] of Object.entries(score)) {
+      if (mappings[key]) {
+          //console.log(`Mapping key: ${key}, value: ${value}`);
+          //console.log(`Mapping function: ${mappings[key].mapFunction}`);
+        mappedScore[mappings[key].parameter] = mappings[key].mapFunction(value);
+      } else {
+        mappedScore[key] = value;
       }
-      return mappedScore;
-    });
-  
-    return children(mappedScores);
-  };
-  export default ScoreMapper;
+    }
+    return mappedScore;
+  });
+
+return children(mappedScores);
+};
+
+export default ScoreMapper;
