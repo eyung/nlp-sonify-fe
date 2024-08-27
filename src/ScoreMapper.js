8ee7b3112f470997a02895fdd66cbf559062cfe3
eyung <effiamyung@gmail.com>
2024-08-27 10:11:23 -0700

    debug log

--- src/ScoreMapper.js
+++ src/ScoreMapper.js
@@ -18,8 +18,8 @@ const ScoreMapper = ({ mappings, children }) => {
     // Map each score 
     for (const [key, value] of Object.entries(score)) {
       if (mappings[key]) {
-          //console.log(`Mapping key: ${key}, value: ${value}`);
-          //console.log(`Mapping function: ${mappings[key].mapFunction}`);
+          console.log(`Mapping key: ${key}, value: ${value}`);
+          console.log(`Mapping function: ${mappings[key].mapFunction}`);
         mappedScore[mappings[key].parameter] = mappings[key].mapFunction(value);
       } else {
         mappedScore[key] = value;
