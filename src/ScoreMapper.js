aaa92dd0ed5dbad010ef0adf41ce745a06a62efa
eyung <effiamyung@gmail.com>
2024-08-27 10:23:59 -0700

    test

--- src/ScoreMapper.js
+++ src/ScoreMapper.js
@@ -17,10 +17,10 @@ const ScoreMapper = ({ mappings, children }) => {
     const mappedScore = {};
 
     // Map each score 
-    for (const [key, value] of Object.entries(wordScores)) {
+    for (const [key, value] of Object.entries(score)) {
       if (mappings[key]) {
-        console.log(`Mapping key: ${key}, value: ${value}`);
-        console.log(`Mapping function: ${mappings[key].mapFunction}`);
+          console.log(`Mapping key: ${key}, value: ${value}`);
+          console.log(`Mapping function: ${mappings[key].mapFunction}`);
         mappedScore[mappings[key].parameter] = mappings[key].mapFunction(value);
       } else {
         mappedScore[key] = value;
