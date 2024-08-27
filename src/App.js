5f44115de4ca4013d0a8612cb5f08939041973d2
eyung <effiamyung@gmail.com>
2024-08-27 15:14:55 -0700

    remove div wrapper around ScoreMapper

--- src/App.js
+++ src/App.js
@@ -162,21 +162,21 @@ const App = ({ setIsLoading }) => {
           </div>
         </DndContext>
 
-        <div className="flex justify-around mt-4">
-          {shouldPlaySound && (
-            <ScoreMapper mappings={mappings}>
-              {(mappedScores) => (
-                <>
-                  <ScoreGraph mappedScores={mappedScores} />
-                  {shouldPlaySound && <SoundPlayer mappedScores={mappedScores} onSoundPlayed={() => setShouldPlaySound(false)} />}
-                  
-                  
-    
-                </>
-              )}
-            </ScoreMapper>
-          )}
-        </div>
+        
+        {shouldPlaySound && (
+          <ScoreMapper mappings={mappings}>
+            {(mappedScores) => (
+              <>
+                <ScoreGraph mappedScores={mappedScores} />
+                {shouldPlaySound && <SoundPlayer mappedScores={mappedScores} onSoundPlayed={() => setShouldPlaySound(false)} />}
+                
+                
+  
+              </>
+            )}
+          </ScoreMapper>
+        )}
+       
 
         <div className="grid grid-cols-4 gap-4 m-10">
         </div>
