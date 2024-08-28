015e6d8ee2831d783307dbce44c252269686734e
eyung <effiamyung@gmail.com>
2024-08-28 15:19:56 -0700

    try catch block around iterating mappedScores in ScoreMapper

--- src/ScoreMapper.js
+++ src/ScoreMapper.js
@@ -7,26 +7,29 @@ const ScoreMapper = ({ mappings, children }) => {
 
   console.log('ScoreMapper: scoresData:', scoresData);
 
-  // Extract and map scores to the desired parameters
-  const mappedScores = scoresData.sentences.map(sentence => {
-    const { word, ...wordScores } = sentence;
-    const mappedScore = { word };
+  let mappedScores = [];
+  try {
+    // Extract and map scores to the desired parameters
+    mappedScores = scoresData.sentences.map(sentence => {
+      const { word, ...wordScores } = sentence;
+      const mappedScore = { word };
 
-    //console.log('wordScores keys:', Object.keys(wordScores));
-    //console.log('mappings keys:', Object.keys(mappings));
-
-    // Map each score 
-    for (const [key, value] of Object.entries(wordScores)) {
-      if (mappings[key]) {
+      // Map each score 
+      for (const [key, value] of Object.entries(wordScores)) {
+        if (mappings[key]) {
           console.log(`Mapping key: ${key}, value: ${value}`);
           console.log(`Mapping function: ${mappings[key].mapFunction}`);
-        mappedScore[mappings[key].parameter] = mappings[key].mapFunction(value);
-      } else {
-        mappedScore[key] = value;
+          mappedScore[mappings[key].parameter] = mappings[key].mapFunction(value);
+        } else {
+          mappedScore[key] = value;
+        }
       }
-    }
-    return mappedScore;
-  });
+      return mappedScore;
+    });
+  } catch (error) {
+    console.error('Error mapping scores:', error);
+    // Optionally, you can set a fallback state or show an error message to the user
+  }
 
 return children(mappedScores);
 };
