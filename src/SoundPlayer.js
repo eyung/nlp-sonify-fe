1cffbbd580eb00ee78e79eda5a00e94657453517
eyung <effiamyung@gmail.com>
2024-09-03 16:49:00 -0700

    test

--- src/SoundPlayer.js
+++ src/SoundPlayer.js
@@ -101,8 +101,8 @@ const SoundPlayer = ({ mappedScores, onSoundPlayed }) => {
 
           synth.volume.value = volume;
           console.log('synth.volume.value:', volume);
-          synth.detune.value = scoreObj.detune; 
-          console.log('synth.detune.value:', scoreObj.detune);
+          //synth.detune.value = scoreObj.detune; 
+          //console.log('synth.detune.value:', scoreObj.detune);
 
           //progression.forEach((chord, chordIndex) => {
           await synth.triggerAttackRelease(
