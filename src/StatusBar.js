9ee1d5d15241502f50a13546e583f7f789b2607a
eyung <effiamyung@gmail.com>
2024-09-05 16:19:11 -0700

    refactor: Remove commented out console.log in StatusBar.js

--- src/StatusBar.js
+++ src/StatusBar.js
@@ -4,7 +4,7 @@ import { useCurrentSentence } from './CurrentSentenceContext';
 const StatusBar = () => {
   const { currentSentence } = useCurrentSentence();
 
-  console.log('Rendering StatusBar with currentSentence:', currentSentence);
+  //console.log('Rendering StatusBar with currentSentence:', currentSentence);
 
   return (
     <div className="fixed bottom-0 w-full bg-gray-800 text-white text-center p-2">
