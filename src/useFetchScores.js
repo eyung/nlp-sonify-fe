3cd14f214e7d6f84a230b93b0b5a59a2e42ae5a7
eyung <effiamyung@gmail.com>
2024-09-05 15:19:20 -0700

    refactor: Update useFetchScores to use useScores context

--- src/useFetchScores.js
+++ src/useFetchScores.js
@@ -1,10 +1,12 @@
 import { useCallback } from 'react';
 import { useAppStateContext } from './AppStateContext';
 import { useAppState } from './hooks/useAppState';
+import { useScores } from './ScoreContext';
 
 export const useFetchScores = () => {
     const webURL = 'https://nlp-sonify-be.vercel.app'; 
-    const { setScoresData, setIsLoading } = useAppStateContext();
+    const { setIsLoading } = useAppStateContext();
+    const { setScoresData } = useScores();
     const { setShowScoreMapper, setShowScoreGraph, setShouldPlaySound } = useAppState();
 
     const fetchScores = useCallback(async (data) => {
