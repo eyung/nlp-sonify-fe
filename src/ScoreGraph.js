03a1e10b0616258a454e9b2e56c2975fa144d797
eyung <effiamyung@gmail.com>
2024-09-09 13:45:01 -0700

    refactor: Update XAxis dataKey in ScoreGraph to fix rendering issue

--- src/ScoreGraph.js
+++ src/ScoreGraph.js
@@ -31,7 +31,7 @@ const ScoreGraph = React.memo(() => {
       <ResponsiveContainer width="100%" height={400}>
         <LineChart data={data}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="" />
           <YAxis />
           <Tooltip />
           <Legend />
