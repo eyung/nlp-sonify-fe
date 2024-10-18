65e97480f9f89a1815e0275c78eac97077dd3830
eyung <effiamyung@gmail.com>
2024-10-18 16:06:53 -0700

    Refactor Container component to set a fixed height for the content

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,7 +7,7 @@ type ContainerProps = {
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-10">
-      <div className="bg-white shadow-lg rounded-lg p-8 max-w-screen-lg w-full h-96 overflow-auto">
+      <div className="bg-white shadow-lg rounded-lg p-8 max-w-screen-lg w-full h-110 overflow-auto">
             {children} 
         </div> 
     </div> 
