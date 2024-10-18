380823aefb8c45b55f5f9e61a5423c5baaf0a1db
eyung <effiamyung@gmail.com>
2024-10-18 15:59:34 -0700

    Refactor Container component to add max-height and overflow-auto

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,11 +7,11 @@ type ContainerProps = {
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-10">
-      <div className="bg-white shadow-lg rounded-lg p-8 max-w-screen-lg w-full">
-        {children}
-      </div>
-    </div>
-  );
+        <div className="bg-white shadow-lg rounded-lg p-8 max-w-screen-lg w-full max-h-screen overflow-auto"> 
+            {children} 
+        </div> 
+    </div> 
+  ); 
 };
 
 export default Container;
