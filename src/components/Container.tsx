df5d7661a28b37bc5b84218f389aea8015797f28
eyung <effiamyung@gmail.com>
2024-10-21 13:28:53 -0700

    Refactor Container component to update layout and height for the content

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,11 +6,11 @@ type ContainerProps = {
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-10">
-      <div className="bg-white shadow-lg rounded-lg p-8 max-w-screen-lg w-full flex flex-col">
+    <div className="flex items-center justify-center min-h-screen p-4 bg-gray-100">
+      <div className="bg-white shadow-lg rounded-lg p-8 max-w-screen-lg w-full">
         {children}
       </div>
-    </div> 
+    </div>
   ); 
 };
 
