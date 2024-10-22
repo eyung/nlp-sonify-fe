436c7f264c5a329ba09901ebdb9c04e0567ad5cd
eyung <effiamyung@gmail.com>
2024-10-22 14:52:09 -0700

    Refactor Container component to update layout and remove unnecessary flex properties

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,11 +6,11 @@ type ContainerProps = {
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="flex items-center justify-center min-h-screen m-4 bg-gray-100">
+  
       <div className="bg-white shadow-lg rounded-lg p-8 max-w-screen-lg w-full">
         {children}
       </div>
-    </div>
+
   ); 
 };
 
