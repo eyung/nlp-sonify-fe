15ad5d5e69bb7a8b90128ef6e5df585cace1dc80
eyung <effiamyung@gmail.com>
2025-04-01 13:45:48 -0700

    Refactor StatusBar component to update background color from gray-500 to gray

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -7,7 +7,7 @@ const StatusBar = () => {
   //console.log('Rendering StatusBar with currentSentence:', currentSentence);
 
   return (
-    <div className="fixed bottom-0 w-full bg-gray-500 text-white text-center p-2">
+    <div className="fixed bottom-0 w-full bg-gray text-white text-center p-2">
       {currentSentence ? `Playing: ${currentSentence}` : ' '}
     </div>
   );
